dddac89677e4b4866b0da7f8c376109be76ac653
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2024-11-27 06:42:39 +0000

    Add support for iterables for chunkify & flatten

--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -1,6 +1,6 @@
 """Tests for chunkify function."""
 
-from collections.abc import Sequence
+from collections.abc import Iterable
 from typing import Any
 
 import pytest
@@ -16,17 +16,21 @@ from orval import chunkify, flatten
         ([1, 2, 3], 5, [[1, 2, 3]]),  # Chunk size larger than list
         ([1, 2, 3, 4], 1, [[1], [2], [3], [4]]),  # Chunk size of 1
         ([1, 2, 3], 3, [[1, 2, 3]]),  # Chunk size equal to list length
+        ({1, 2, 3}, 2, [[1, 2], [3]]),  # Set
+        (range(5), 2, [[0, 1], [2, 3], [4]]),  # Range
+        (("a", "b", "c"), 2, [["a", "b"], ["c"]]),  # Tuple
     ],
 )
-def test_chunkify(sequence: list[int], size: int, expected: list[list[int]]) -> None:
+def test_chunkify(sequence: Iterable[Any], size: int, expected: list[list[Any]]) -> None:
     """Should return a list of chunks of the given size."""
     assert chunkify(sequence, size) == expected
 
 
-def test_chunkify_invalid_size() -> None:
+@pytest.mark.parametrize("size", [-1, 0])
+def test_chunkify_invalid_size(size: int) -> None:
     """Should raise a ValueError for invalid size."""
-    with pytest.raises(ValueError, match="Size must be > 0, invalid value 0"):
-        chunkify([1, 2, 3], 0)
+    with pytest.raises(ValueError, match=f"Size must be > 0, invalid value {size}"):
+        chunkify([1, 2, 3], size)
 
 
 @pytest.mark.parametrize(
@@ -46,7 +50,7 @@ def test_chunkify_invalid_size() -> None:
         (["a", ["b", "c"]], None, ["a", "b", "c"]),
     ],
 )
-def test_flatten(sequence: Sequence[Any] | set[Any], depth: int | None, expected: list[Any]) -> None:
+def test_flatten(sequence: Iterable[Any] | set[Any], depth: int | None, expected: list[Any]) -> None:
     """Should return a flattened sequence up to the given depth."""
     assert list(flatten(sequence, depth)) == expected
 
@@ -60,5 +64,5 @@ def test_flatten_invalid_depth() -> None:
 @suppress_type_checks
 def test_flatten_invalid_type() -> None:
     """Should raise a TypeError for invalid type."""
-    with pytest.raises(TypeError, match="Input must be a sequence."):
+    with pytest.raises(TypeError, match=r"Input must be an interable \(list, set, range, tuple\)\."):
         list(flatten(1, 1))  # type: ignore[arg-type]
